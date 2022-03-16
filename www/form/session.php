542c34f739a180f7bd21f8b51f7ec0ef4d0aeb3a
MatisFl <fleuriotmatis@gmail.com>
2022-03-16 17:35:39 +0100

    Ajouts divers

--- www/form/session.php
+++ www/form/session.php
@@ -17,12 +17,11 @@ if (!isset($_SESSION["session"])) header("Location: /form/");
 
 <body>
     <p>Nom d'utilisateur : <?php echo $_SESSION["username"] ?></p><br>
-    <p>Hash : <?php echo $_SESSION["password"] ?></p><br>
     <p>Prénom : <?php echo $_SESSION["firstname"] ?></p><br>
     <p>Nom de famille : <?php echo $_SESSION["lastname"] ?></p><br>
     <p>Statut : <?php echo $_SESSION["status"] ?></p><br>
     <p>Mail : <?php echo $_SESSION["email"] ?></p><br>
-
+    <a href="form.php">Formulaire</a><br>
     <a href="disconnect.php">Se déconnecter</a>
 </body>
 
