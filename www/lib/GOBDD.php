316b1940f33d51e78dc754a00df1dbbd63bff9f3
Gregoire-BR <gregoirebrosset17@gmail.com>
2022-03-17 15:26:18 +0100

    Ajout de docs

--- www/lib/GOBDD.php
+++ www/lib/GOBDD.php
@@ -5,8 +5,8 @@
 		/**
 		* @param host - domaine du SGBD
 		* @param db - nom de la base de données
-		* @param user - nom d'utilisateur
-		* @param pswd - mot de passe; à stocker dans un fichier séparé et utilisé
+		* @param user - nom d'utilisateur; à stocker dans un fichier séparé et sécurisé
+		* @param pswd - mot de passe; à stocker dans un fichier séparé et sécurisé
 		*/
         function __construct(string $host, string $db, string $user, string $pswd) {
             try {
@@ -18,6 +18,11 @@
             }
         }
 
+		/**
+		* @brief Cherche un utilisateur et retourne les informations sur lui
+		* @param user - Nom d'utilisateur à chercher
+		* @return rslt - tableau associatif contenant toutes les informations, avec ces paires : <ul><li>username: nom d'utilisateur</li><li>password: mot de passe hashé</li><li>firstname: prénom</li><li>lastname: nom de famille</li><li>status: grade de l'utilisateur; 0: étudiant; 1: enseignant; 2: proviseur adjoint; 3: secrétaire</li><li>email: adresse email</li><ul>
+		*/
 		function userQuery(string $user) {
 			$stmt = $this->bdd->prepare("SELECT * FROM users WHERE username = LOWER(:user)");
 			if(!$stmt) {
@@ -32,6 +37,12 @@
 			return $rslt;
 		}
 
+		/**
+		* @brief Change le mot de passe d'un utilisateur
+		* @param user - nom d'utilisateur
+		* @param pswd - nouveau mot de passe. Ne doit pas être hashé car cette méthode s'en chargera
+		* @return - rowCount de stmt; 1 si succès, 0 si erreur, autre chose si grosse erreur
+		*/
 		function changePassword($user, $pswd) {
 			$stmt = $this->bdd->prepare("UPDATE users SET password=PASSWORD(:pswd) WHERE username = LOWER(:user)");
 			if(!$stmt) {
@@ -44,6 +55,11 @@
 			return $stmt->rowCount();
 		}
 
+		/**
+		* @brief Cherche un étudiant et retourne les informations sur lui
+		* @param user - Nom d'utilisateur de l'étudiant à chercher
+		* @return rslt - tableau associatif contenant toutes les informations, avec ces paires : <ul><li>username: nom d'utilisateur</li><li>ine: identifiant INE crypté</li><li>spec1: spécialité 1</li><li>spec2: spécialité 2</li><li>ens1: nom d'utilisateur de l'enseignant 1</li><li>ens2: nom d'utilisateur de l'enseignant 2</li><li>etabville: etablissement et ville; peut-être inutile</li><ul>
+		*/
 		function studentQuery($user){
 			$stmt = $this->bdd->prepare("SELECT * FROM students WHERE username = LOWER(:user)");
 			if(!$stmt) {
@@ -58,6 +74,12 @@
 			return $rslt;
 		}
 
+		/**
+		* @brief Vérifie la validité d'une paire d'identifiants. Cherche si il existe un compte avec le même nom d'utilisateur et mot de passe
+		* @param user - nom d'utilisateur
+		* @param pswd - mot de passe
+		* @rslt - 1 si il y a un (seul) compte correspondant; 0 si il n'y en a pas (ou si il y en a plus d'un; théoriquement impossible)
+		*/
 		function checkCredentials($user,$pswd) {
 			$user = userQuery($user);
 			$stmt = $this->bdd->prepare("SELECT * FROM users WHERE username = LOWER(:user) AND password=PASSWORD(:pswd)");
