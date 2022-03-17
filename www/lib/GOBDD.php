653ea3cb1862886a035e3e05b206f3e85474268e
Gregoire-BR <gregoirebrosset17@gmail.com>
2022-03-17 15:13:43 +0100

    Fix: LOWER($user) -> strtolower($user)

--- www/lib/GOBDD.php
+++ www/lib/GOBDD.php
@@ -2,6 +2,12 @@
     class GOBDD {
         private $bdd;
 
+		/**
+		* @param host - domaine du SGBD
+		* @param db - nom de la base de données
+		* @param user - nom d'utilisateur
+		* @param pswd - mot de passe; à stocker dans un fichier séparé et utilisé
+		*/
         function __construct(string $host, string $db, string $user, string $pswd) {
             try {
                 $this->bdd = new PDO('mysql:host='.$host.';dbname='.$db.';charset=utf8',$user,$pswd);
@@ -12,56 +18,13 @@
             }
         }
 
-        /**
-        * @brief Tire toutes les infos d'une entrée dans une table donnée, sélectionnée selon une valeur et une colonne données
-        * @param table - table dans laquelle chercher
-        * @param col - colonne selon laquelle chercher
-        * @param id - valeur identifiante à chercher;
-        * @return rslt - array contenant les colonnes en clé et les valeurs
-        */
-        /*function generalSelect(string $table, string $col, int $id) {
-            $request = "SELECT * FROM `".$table."` WHERE ".$col."=:id/".$id."<br>";
-            $stmt = $this->bdd->prepare($request);
-            if (!$stmt) {
-                echo "\nPDO::errorInfo():\n";
-                echo $bdd->errorInfo();
-            }
-            //echo $stmt->bindParam(':id',$id,PDO::PARAM_INT)."<br>";
-        	if(!$stmt->execute()){
-                var_dump($stmt->errorInfo());
-            }
-            //var_dump($stmt);
-        	$rslt = $stmt->fetch(PDO::FETCH_ASSOC); // sort un array clé-valeur
-            return $rslt;
-        }*/
-
-        /**
-        * @brief Fonction générale pour INSERT
-        */
-		/*
-        function generalInsert(string $table, array $data) {
-            $q = "INSERT INTO".$table."(";
-            foreach($data as $a => $b) {
-                $q .= $a.",";
-            }
-            $q .= ") VALUES (";
-            foreach($data as $a => $b) {
-                $q .=  ":".$a.",";
-            }
-            $stmt = $this->bdd->prepare($q);
-            foreach($data as $a => $b) {
-                $stmt->bindParam(":".$a,$b);
-            }
-            $stmt->execute();
-        }*/
-
 		function userQuery(string $user) {
 			$stmt = $this->bdd->prepare("SELECT * FROM users WHERE username = LOWER(:user)");
 			if(!$stmt) {
 				echo "\nPDO::errorInfo():\n";
 				echo $bdd->errorInfo();
 			}
-			$stmt->bindParam(':user',LOWER($user),PDO::PARAM_STR);
+			$stmt->bindParam(':user',strtolower($user),PDO::PARAM_STR);
 			if(!$stmt->execute()){
 				var_dump($stmt->errorInfo());
 			}
@@ -75,7 +38,7 @@
 				echo "\nPDO::errorInfo():\n";
 				echo $bdd->errorInfo();
 			}
-			$stmt->bindParam(':user',LOWER($user),PDO::PARAM_STR);
+			$stmt->bindParam(':user',strtolower($user),PDO::PARAM_STR);
 			$stmt->bindParam(':pswd',$pswd,PDO::PARAM_STR);
 			$stmt->execute();
 			return $stmt->rowCount();
@@ -87,7 +50,7 @@
 				echo "\nPDO::errorInfo():\n";
 				echo $bdd->errorInfo();
 			}
-			$stmt->bindParam(':user',LOWER($user),PDO::PARAM_STR);
+			$stmt->bindParam(':user',strtolower($user),PDO::PARAM_STR);
 			if(!$stmt->execute()){
 				var_dump($stmt->errorInfo());
 			}
@@ -98,7 +61,7 @@
 		function checkCredentials($user,$pswd) {
 			$user = userQuery($user);
 			$stmt = $this->bdd->prepare("SELECT * FROM users WHERE username = LOWER(:user) AND password=PASSWORD(:pswd)");
-			$stmt->bindParam(':user',LOWER($user),PDO::PARAM_STR);
+			$stmt->bindParam(':user',strtolower($user),PDO::PARAM_STR);
 			$stmt->bindParam(':pswd',$pswd,PDO::PARAM_STR);
 			$stmt->execute();
 			return ($stmt->rowCount() == 1 ? 1 : 0);
@@ -106,7 +69,7 @@
 
 		function formHistoryQuery(string $user) {
 			$stmt = $this->bdd->prepare("SELECT * FROM forms WHERE username = LOWER(:user);");
-			$stmt->bindParam(':user',LOWER($user),PDO::PARAM_STR);
+			$stmt->bindParam(':user',strtolower($user),PDO::PARAM_STR);
 			$stmt->execute();
 			$rslt = $stmt->fetch(PDO::FETCH_ASSOC);   // sort un array clé-valeur
 			return $rslt;
@@ -114,7 +77,7 @@
 
 		function formQuery($user) {
 			$stmt = $this->bdd->prepare("SELECT TOP 1 * from forms WHERE username = LOWER(:user) ORDER BY date DESC;");
-			$stmt->bindParam(':user',LOWER($user),PDO::PARAM_STR);
+			$stmt->bindParam(':user',strtolower($user),PDO::PARAM_STR);
 			$stmt->execute();
 			$rslt = $stmt->fetch(PDO::FETCH_ASSOC);   // sort un array clé-valeur
 			return $rslt;
@@ -129,7 +92,7 @@
 		*/
 		function updateForm(string $user, string $q1, string $q2) {
 			$stmt = $this->bdd->prepare("UPDATE table SET q1=:q1, q2=:q2 WHERE (SELECT TOP 1 * from forms WHERE username = LOWER(:user) ORDER BY date DESC);");
-			$stmt->bindParam(':user',LOWER($user),PDO::PARAM_STR);
+			$stmt->bindParam(':user',strtolower($user),PDO::PARAM_STR);
 			$stmt->bindParam(':q1',$q1,PDO::PARAM_STR);
 			$stmt->bindParam(':q2',$q2,PDO::PARAM_STR);
 			$stmt->execute();
@@ -145,13 +108,13 @@
 		*/
 		function updateStudent() {
 			$stmt = $this->bdd->prepare("UPDATE table SET spec1=:spec1, spec2=:spec2 WHERE (SELECT TOP 1 * from forms WHERE username = LOWER(:user) ORDER BY date DESC)");
-			$stmt->bindParam(':user',LOWER($user),PDO::PARAM_STR);
+			$stmt->bindParam(':user',strtolower($user),PDO::PARAM_STR);
 			$stmt->bindParam(':spec1',$spec1,PDO::PARAM_STR);
 			$stmt->bindParam(':spec2',$spec2,PDO::PARAM_STR);
 			$stmt->execute();
 			return $stmt->rowCount();;
 		}
 
-		/*function swapForm() {}*/ // TODO: 
+		/*function swapForm() {}*/ // TODO:
     }
 ?>
