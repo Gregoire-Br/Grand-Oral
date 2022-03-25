daf86710dce1667d8263bfc4ed3a45de86faf94e
Gregoire-BR <gregoirebrosset17@gmail.com>
2022-03-25 13:44:10 +0100

    Fix: mauvais chemin vers stylesheet

--- www/form/form.php
+++ www/form/form.php
@@ -31,9 +31,9 @@ $forminfo = $bdd->formQuery($_SESSION["username"]);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="/form/style/style.css">
+    <link rel="stylesheet" href="style/style.css">
     <link rel="icon" type="image/png" href="/form/img/favicon.png" />
-    
+
     <script src="js/bootstrap.js"></script>
     <script src="js/formvalidation.js"></script>
     <script src="js/alertHandler.js"></script>
