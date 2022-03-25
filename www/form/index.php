45dc777b1bca21f4d2fe4975d294f0e0cc6875d7
Gregoire-BR <gregoirebrosset17@gmail.com>
2022-03-25 13:56:38 +0100

    Fix: mauvais chemin vers favicon

--- www/form/index.php
+++ www/form/index.php
@@ -42,7 +42,7 @@ if ($_POST["submit"]) {
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="style/style.css">
-    <link rel="icon" type="image/png" href="/form/img/favicon.png" />
+    <link rel="icon" type="image/png" href="/img/favicon.png" />
 
     <script src="js/bootstrap.js"></script>
     <script src="js/formvalidation.js"></script>
