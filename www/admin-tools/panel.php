8ed6f08925a9f33baf4ca1d41b7a2ad3ddb82ebe
Gregoire-BR <gregoirebrosset17@gmail.com>
2022-03-25 10:07:55 +0100

    Interface

--- www/admin-tools/panel.php
+++ www/admin-tools/panel.php
@@ -9,91 +9,177 @@
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	</head>
-	<ul class="nav nav-tabs" id="tabList" role="tablist">
 	<body>
-		  <li class="nav-item">
-		    <a class="nav-link active" id="tab-forms" data-toggle="tab" href="#forms" role="tab" aria-controls="forms" aria-selected="true">Formulaires</a>
-		  </li>
-		  <li class="nav-item">
-		    <a class="nav-link" id="tab-profiles" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profils</a>
-		  </li>
-		  <li class="nav-item">
-		    <a class="nav-link" id="tab-imports" data-toggle="tab" href="#imports" role="tab" aria-controls="imports" aria-selected="false">Imports</a>
-		  </li>
+		<ul class="nav nav-tabs" id="tabList" role="tablist">
+			<li class="nav-item">
+				<a class="nav-link active" id="tab-mainTab" data-toggle="tab" href="#mainTab" role="tab" aria-controls="mainTab" aria-selected="true">Tableau de bord</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" id="tab-forms" data-toggle="tab" href="#forms" role="tab" aria-controls="forms" aria-selected="false">Formulaires</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" id="tab-profiles" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profils</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" id="tab-imports" data-toggle="tab" href="#imports" role="tab" aria-controls="imports" aria-selected="false">Imports</a>
+			</li>
 		</ul>
 		<div class="tab-content" id="tabContent">
-		  <div class="tab-pane fade show active" id="forms" role="tabpanel" aria-labelledby="forms-tab">
-			  <h4>Fiches à valider</h4>
-			  <table class="table table-hover table-sm table-striped">
-	  			<thead>
-	  			  <tr>
-	  				<th scope="col">Nom</th>
-	  				<th scope="col">Date</th>
-	  				<th scope="col">Questions</th>
-	  				<th scope="col">Valider ?</th>
-	  			  </tr>
-	  			</thead>
-	  			<tbody>
-	  			  <tr>
+			<div class="tab-pane fade show active" id="mainTab" role="tabpanel" aria-labelledby="mainTab-tab">
+				<h4>Fiches à valider</h4>
+				<table class="table table-hover table-sm table-striped">
+		  			<thead>
+		  			  <tr>
+		  				<th scope="col">Nom</th>
+		  				<th scope="col">Date</th>
+		  				<th scope="col">Formulaire</th>
+						<th scope="col">État</th>
+		  				<th scope="col">Valider ?</th>
+		  			  </tr>
+		  			</thead>
+		  			<tbody>
+		  			  <tr class="list-form">
+						<th scope="row">Garfield</td>
+						<td>21/10/2022, 10h34</td>
+						<td><b>Q°1 - Sciences et Vie de la Terre</b><br>
+								<p>Comment est-ce que les plantes respirent?</p><br>
+							<b>Q°2 - Sciences et Vie de la Terre/Physiques & Chimie</b><br>
+								<p>Qu'est-ce qui est contenu dans l'air ?</p>
+						</td>
+						<td>
+							<b>M.DUPOND : Validé</b><br>
+							<b>M.HADDOCK : Validé</b><br>
+							M.DUPONT : à valider
+						</td>
+		  				<td>
+							<button type="button" class="btn btn-success">Y</button>
+							<button type="button" class="btn btn-danger">N</button>
+						</td>
+		  			  </tr>
+					  <tr class="list-form">
+						  <th scope="row">Garfield</td>
+						  <td>21/10/2022, 10h34</td>
+						  <td><b>Q°1 - Sciences et Vie de la Terre</b><br>
+								  <p>Comment est-ce que les plantes respirent?</p><br>
+							  <b>Q°2 - Sciences et Vie de la Terre/Physiques & Chimie</b><br>
+								  <p>Qu'est-ce qui est contenu dans l'air ?</p>
+						  </td>
+						  <td>
+							  <b>M.DUPOND : Validé</b><br>
+							  <b>M.HADDOCK : Validé</b><br>
+							  M.DUPONT : à valider
+						  </td>
+						  <td>
+							  <button type="button" class="btn btn-success">Y</button>
+							  <button type="button" class="btn btn-danger">N</button>
+						  </td>
+					  </tr>
+					  <tr class="list-form">
+						  <th scope="row">Garfield</td>
+						  <td>21/10/2022, 10h34</td>
+						  <td><b>Q°1 - Sciences et Vie de la Terre</b><br>
+								  <p>Comment est-ce que les plantes respirent?</p><br>
+							  <b>Q°2 - Sciences et Vie de la Terre/Physiques & Chimie</b><br>
+								  <p>Qu'est-ce qui est contenu dans l'air ?</p>
+						  </td>
+						  <td>
+							  <b>M.DUPOND : Validé</b><br>
+							  <b>M.HADDOCK : Validé</b><br>
+							  M.DUPONT : à valider
+						  </td>
+						  <td>
+							  <button type="button" class="btn btn-success">Y</button>
+							  <button type="button" class="btn btn-danger">N</button>
+						  </td>
+					  </tr>
+		  			</tbody>
+		  		  </table>
+				  <h4>Activités des fiches</h4>
+				  <table class="table table-hover table-sm table-striped">
+		  			<thead>
+		  			  <tr>
+		  				<th scope="col">Date</th>
+		  				<th scope="col">Description</th>
+		  			  </tr>
+		  			</thead>
+		  			<tbody>
+		  				<tr class="act act-bad bg-danger text-white">
+		  					<td>21/10/2022, 10h34</td>
+		  					<td><b>M.DUPOND</b> a refusé la fiche de <b>Jean</b></td>
+		  				</tr>
+		  				<tr class="act act-good bg-success text-white">
+		  					<td>21/10/2022, 10h32</td>
+		  					<td><b>M.DUPONT</b> a validé la fiche de <b>Jean</b></td>
+		  				</tr>
+		  				<tr class="act act-neut">
+		  					<td>21/10/2022, 10h30</td>
+		  					<td><b>Jean</b> a mis à jour sa fiche</td>
+		  				</tr>
+		  			</tbody>
+		  		  </table>
+			</div>
+			<div class="tab-pane fade" id="forms" role="tabpanel" aria-labelledby="forms-tab">
+				  <table class="table table-hover table-sm table-striped">
+					<thead>
+					  <tr>
+						<th scope="col">Nom</th>
+						<th scope="col">Date</th>
+						<th scope="col">Formulaire</th>
+						<th scope="col">État</th>
+					  </tr>
+					</thead>
+					<tbody>
+						<tr class="list-form">
+							<th scope="row">Garfield</td>
+							<td>21/10/2022, 10h34</td>
+							<td><b>Q°1 - Sciences et Vie de la Terre</b><br>
+									<p>Comment est-ce que les plantes respirent?</p><br>
+								<b>Q°2 - Sciences et Vie de la Terre/Physiques & Chimie</b><br>
+									<p>Qu'est-ce qui est contenu dans l'air ?</p>
+							</td>
+							<td>
+								<b>M.DUPOND : Validé</b><br>
+								<b>M.HADDOCK : Validé</b><br>
+								M.DUPONT : à valider
+							</td>
+						</tr>
+						<tr class="list-form">
+							<th scope="row">Garfield</td>
+							<td>21/10/2022, 10h34</td>
+							<td><b>Q°1 - Sciences et Vie de la Terre</b><br>
+									<p>Comment est-ce que les plantes respirent?</p><br>
+								<b>Q°2 - Sciences et Vie de la Terre/Physiques & Chimie</b><br>
+									<p>Qu'est-ce qui est contenu dans l'air ?</p>
+							</td>
+							<td>
+								<b>M.DUPOND : Validé</b><br>
+								<b>M.HADDOCK : Validé</b><br>
+								M.DUPONT : à valider
+							</td>
+						</tr>
+						<tr class="list-form">
+							<th scope="row">Garfield</td>
+							<td>21/10/2022, 10h34</td>
+							<td><b>Q°1 - Sciences et Vie de la Terre</b><br>
+									<p>Comment est-ce que les plantes respirent?</p><br>
+								<b>Q°2 - Sciences et Vie de la Terre/Physiques & Chimie</b><br>
+									<p>Qu'est-ce qui est contenu dans l'air ?</p>
+							</td>
+							<td>
+								<b>M.DUPOND : Validé</b><br>
+								<b>M.HADDOCK : Validé</b><br>
+								M.DUPONT : à valider
+							</td>
+						</tr>
+					</tbody>
+				  </table>
+			  </div>
+			<div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
 
-	  				<th scope="row">Jean</th>
-	  				<td>21/10/2022, 10h30</td>
-	  				<td> <!-- TODO:  --> </td>
-	  				<td>
-						<button type="button" class="btn btn-success">Y</button>
-						<button type="button" class="btn btn-danger">N</button>
-					</td>
-	  			  </tr>
-	  			  <tr>
-	  				<th scope="row">Jean</th>
-	  				<td>21/10/2022, 10h30</td>
-	  				<td> <!-- TODO:  --> </td>
-					<td>
-						<button type="button" class="btn btn-success">Y</button>
-						<button type="button" class="btn btn-danger">N</button>
-					</td>
-	  			  </tr>
-	  			  <tr>
-	  				<th scope="row">Jean</th>
-	  				<td>21/10/2022, 10h30</td>
-	  				<td> <!-- TODO:  --> </td>
-					<td>
-						<button type="button" class="btn btn-success">Y</button>
-						<button type="button" class="btn btn-danger">N</button>
-					</td>
-	  			  </tr>
-	  			</tbody>
-	  		  </table>
-			  <h4>Activités des fiches</h4>
-			  <table class="table table-hover table-sm table-striped">
-	  			<thead>
-	  			  <tr>
-	  				<th scope="col">Date</th>
-	  				<th scope="col">Description</th>
-	  			  </tr>
-	  			</thead>
-	  			<tbody>
-	  				<tr class="act-bad bg-danger text-white">
-	  					<td>21/10/2022, 10h34</td>
-	  					<td><b>M.DUPOND</b> a refusé la fiche de <b>Jean</b></td>
-	  				</tr>
-	  				<tr class="act-good bg-success text-white">
-	  					<td>21/10/2022, 10h32</td>
-	  					<td><b>M.DUPONT</b> a validé la fiche de <b>Jean</b></td>
-	  				</tr>
-	  				<tr class="act-neut">
-	  					<td>21/10/2022, 10h30</td>
-	  					<td><b>Jean</b> a mis à jour sa fiche</td>
-	  				</tr>
-	  			</tbody>
-	  		  </table>
-		  </div>
-		  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+			</div>
+			<div class="tab-pane fade" id="imports" role="tabpanel" aria-labelledby="imports-tab">
 
-		  </div>
-		  <div class="tab-pane fade" id="imports" role="tabpanel" aria-labelledby="imports-tab">
-
-		  </div>
+			</div>
 	  	</div>
 	</body>
 </html>
