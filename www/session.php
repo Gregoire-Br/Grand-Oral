f493d8f28d24b83cd5873f3909725cad31c31444
Gregoire-Br <98598178+Gregoire-Br@users.noreply.github.com>
2022-04-15 14:34:23 +0200

    Delete www directory

--- www/session.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION["session"])) header("Location: /");
-else if($_SESSION["status"] == 0) header("Location: /form.php");
-else if($_SESSION["status"] == 1) header("Location: /board.php");
-else if($_SESSION["status"] == 2) header("Location: /board.php");
-else if($_SESSION["status"] == 3) header("Location: /board.php");
-?>
--- www/var/sql.php
+++
@@ -1,5 +0,0 @@
-<?php
-$sql_ip = '127.0.0.1';
-$sql_login = 'root';
-$sql_password = 'root';
-$sql_db = 'grandoral';
