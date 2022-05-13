50a3f19c3107d36cfe674a06662430fb1f3e0169
Maxime <maxime.pouzargue7@gmail.com>
2022-05-13 14:53:49 +0200

    [V1.0] CSV /XML + Base Siècle ( Petite modif form)

---
+++ csv/csvDecoderV1.0.php
@@ -0,0 +1,61 @@
+  <h2>Insert CSV Data to MySql Table Output</h2>
+
+<?php
+
+/*Création de comptes utilisateurs "ENSEIGNANT" dans la BDD à l'aide un import CSV*/
+
+include "sql.php";
+include "GOBDD.php";
+
+$bdd = new GOBDD($sql_ip, $sql_db, $sql_login, $sql_password);
+
+  function random_password( $length = 8 ) {
+      $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&?";
+      $password = substr( str_shuffle( $chars ), 0, $length );
+      return $password;
+  }
+
+//Ouverture du fichier x.CSV
+if(($fileHandle = fopen("example.csv", "r")) !==FALSE);{
+
+  $status = 1;
+  $password = "";
+  $username = "";
+
+  while (list($firstname, $lastname, $email) = fgetcsv($fileHandle, 0, ';')){
+
+      $password = random_password(8);
+
+      echo"<br>";
+      echo $firstname. "\t" .$lastname. "\t" .$email;
+      echo"<br>";
+
+      echo $status;
+      echo "<br>";
+
+        /*  substr — Retourne un segment de chaîne
+            strtolower - Transforme une chaine de charactères en charactères minuscules
+            homonys - Méthode GOBDD qui incrémente la fin du string $username +1 si déjà existant
+        */
+
+      $username = substr($firstname, 0, 1). "." .$lastname;
+
+      // SELECT * FROM users WHERE username LIKE "1(firstname).lastname%"
+      $dupes = $bdd->homonyms($username);
+      var_dump($dupes);
+      if($dupes >= 1) {
+        $username = $username.$dupes;
+      }
+      $username = strtolower($username);
+
+      echo $username;
+      echo "<br>";
+      
+      if(!$bdd->createUser($username, $password, $firstname, $lastname, $status, $email)) {
+        echo "Echec à l'import de $username<br>";
+    } else {
+      echo "Import des utilisateurs OK <br>";
+    }
+  }
+}
+?>
--- import.php
+++ import.php
@@ -20,20 +20,103 @@ $erreur = '';
 <body>
 	<?php include "var/navbar.html" ?>
     <div class="container d-flex flex-column">
-        <form action="upload.php" method="post" enctype="multipart/form-data">
-            <div class="mb-3">
-                <label for="formFileMultiple" class="form-label">Fichier XML :</label>
-                <input class="form-control" type="file" id="formFileMultiple" multiple>
-                <button type="submit" class="btn btn-primary float-end" name="submit">UPLOAD</button>
+        <form method="post" enctype="multipart/form-data">
+        <label for="formFileMultiple" class="form-label">IMPORT Élèves (Base Siècle) - Format XML UNIQUEMENT (1 Go MAX):</label>
+
+            <div class="mb-3 d-flex">
+                <input class="form-control" type="file" id="fileToUpload" name="userfile" multiple>
+                <button type="submit" class="btn btn-primary" name="submit">UPLOAD</button>
+
             </div>
 
-            <div class="mb-3">
-                <label for="formFileMultiple" class="form-label">Fichier CSV :</label>
+        <label for="formFileMultiple" class="form-label">IMPORT Enseignants - Format CSV UNIQUEMENT :</label>
+    
+            <div class="mb-3 d-flex">
                 <input class="form-control" type="file" id="formFileMultiple" multiple>
+                <button type="submit" class="btn btn-primary" name="csvsubmit">UPLOAD</button>
             </div>
-            <button type="submit" class="btn btn-primary" name="submit">UPLOAD</button>
+            
         </form>
     </div>
+
+<?php
+
+if(isset($_POST['submit']))
+{
+    $file = $_FILES['userfile'];
+    $fileName = $_FILES['userfile']['name'];
+    $fileError = $_FILES['userfile']['error'];
+    $fileSize = $_FILES['userfile']['size'];
+
+    print_r($file);
+    
+    $fileExt= explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+
+    $allowed = array('xml');
+    $feed = file_get_contents($fileName);
+
+    if(in_array($fileActualExt, $allowed)){
+         //if($fileError === 0){
+           // if($fileSize < 1000000){
+               
+            //file_get_content associé à Load_sring . Ne pas utiliser load_file car interprète un objet "Cannot create object"
+                $xml = simplexml_load_string($feed) or die("Error: Cannot create object");
+
+                    $firstname = "";
+                    $lastname = "";
+                    $email = "";
+                    $status = 0;
+                    $username = "";
+                    $password = "";
+
+                foreach($xml->DONNEES->ELEVES->ELEVE as $value) {
+
+                    $firstname = $value->PRENOM;
+                    $lastname = $value->NOM_DE_FAMILLE;
+                    $email = $value->MEL;
+
+                    $password = random_password(8);
+
+                    /*     substr — Retourne un segment de chaîne
+                            strtolower - Transforme une chaine de charactères en charactères minuscules
+                            homonys - Méthode GOBDD qui incrémente la fin du string $username +1 si déjà existant
+                    */
+                    
+                    $username = substr($firstname, 0, 1).$lastname;
+
+                    // SELECT * FROM users WHERE username LIKE "1(firstname).lastname%"
+                    $dupes = $bdd->homonyms($username);
+                    var_dump($dupes);
+                    if($dupes >= 1) {
+                        $username = $username.$dupes;
+                    }
+                    $username = strtolower($username);
+
+                    if(!$bdd->createUser($username, $password, $firstname, $lastname, $status, $email)) {
+                        echo "Echec à l'import de $username<br>";
+                    } else {
+                        echo "Import des utilisateurs OK <br>";
+                    }
+                }
+
+            /* } else {
+                echo "File too big";
+            } */
+
+        /* } else {
+            echo " You got an error ";
+        } */
+
+    } else {
+        echo "Wrong file type";
+    }
+
+}
+
+?>
+
+
         <?php include "var/js.html" ?>
 	</body>
 </html>
