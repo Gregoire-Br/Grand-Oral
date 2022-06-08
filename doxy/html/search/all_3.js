1f1562458f88d94a3ea67564d66b91f9ff67f8e0
Gregoire-Br <gregoirebrosset17@gmail.com>
2022-06-08 17:44:14 +0200

    Mise à jour documentation

--- doxy/html/search/all_3.js
+++ doxy/html/search/all_3.js
@@ -4,7 +4,9 @@ var searchData=
   ['checkcredentials_1',['checkCredentials',['../class_g_o_b_d_d.html#a31d213f221c97e8b9bf8e6ccac7c29a9',1,'GOBDD']]],
   ['classesquery_2',['classesQuery',['../class_g_o_b_d_d.html#a8304a1856dbfe212d79b3ebe738f2ea9',1,'GOBDD']]],
   ['cleanformhistory_3',['cleanFormHistory',['../class_g_o_b_d_d.html#a038b98c22b66006441bdff467f671b80',1,'GOBDD']]],
-  ['createstudent_4',['createStudent',['../class_g_o_b_d_d.html#adc18956a094c5fd962ff7f3836872e89',1,'GOBDD']]],
-  ['createuser_5',['createUser',['../class_g_o_b_d_d.html#aa5a1b8e451b476dee6ab5243f7f8838e',1,'GOBDD']]],
-  ['csvdecoderv1_2e0_2ephp_6',['csvDecoderV1.0.php',['../csv_decoder_v1_80_8php.html',1,'']]]
+  ['cleanorphanforms_4',['cleanOrphanForms',['../class_g_o_b_d_d.html#a1dbdd6e7e5b53a584b3d1330a0fdbe2a',1,'GOBDD']]],
+  ['cleanorphanstudents_5',['cleanOrphanStudents',['../class_g_o_b_d_d.html#ada8606bf13312a1e9e551981b5f1c850',1,'GOBDD']]],
+  ['createstudent_6',['createStudent',['../class_g_o_b_d_d.html#adc18956a094c5fd962ff7f3836872e89',1,'GOBDD']]],
+  ['createuser_7',['createUser',['../class_g_o_b_d_d.html#aa5a1b8e451b476dee6ab5243f7f8838e',1,'GOBDD']]],
+  ['csvdecoderv1_2e0_2ephp_8',['csvDecoderV1.0.php',['../csv_decoder_v1_80_8php.html',1,'']]]
 ];
